400e4d73c8f59d321e4a8db265d866061538ebc1
Supanee <kunnikarboon@gmail.com>
2024-05-27 20:33:01 +0200

    modified mywork_data.js

--- src/assets/mywork_data.js
+++ src/assets/mywork_data.js
@@ -1,6 +1,6 @@
-import project1_img from '../assets/project_1.svg'
-import project2_img from '../assets/project_2.svg'
-import project3_img from '../assets/project_3.svg'
+import project1_img from '../assets/KB-Creativ.png'
+import project2_img from '../assets/Bhromaon.png'
+import project3_img from '../assets/Genesis.png'
 import project4_img from '../assets/project_4.svg'
 import project5_img from '../assets/project_5.svg'
 import project6_img from '../assets/project_6.svg'
@@ -8,32 +8,62 @@ import project6_img from '../assets/project_6.svg'
 const mywork_data = [
     {
         w_no:1,
-        w_name:"Web design",
+        w_name:"KB Creativ Agency",
+        w_desc:"This project is an agency design website. ",
+        w_note:"Note: It is a responsive website. ",
+        w_built:"Built using HTML, CSS, and Bootstrap.",
+        w_more:"This project is an agency design website that showcases the services of the agency. The website is responsive and can be viewed on any device. It was built using HTML, CSS, and Bootstrap.",
+        w_img_url:"https://kunnikarb.github.io/bootstrap/",
         w_img:project1_img
     },
     {
         w_no:2,
-        w_name:"Web design",
+        w_name:"Bhromaon Travel",
+        w_desc:"This project is a travel agency website.",
+        w_note:"Note: It is a responsive website . ",
+        w_built:"Built using HTML, CSS, Sass and Figma.",
+        w_more:"This project is an agency design website that showcases the services of the travel agency. The website is responsive and can be viewed on any device.",
+        w_img_url:"https://kunnikarb.github.io/web-project/",
         w_img:project2_img
     },
     {
         w_no:3,
-        w_name:"Web design",
+        w_name:"Genesis Portfolio",
+        w_desc:"Web development is the process of building, programming...",
+        w_note:"Note: It is a responsive website that can be viewed on any device. ",
+        w_built:"Built using HTML, CSS, and JavaScript",
+        w_more:"This project is an agency design website that showcases the services of the agency. The website is responsive and can be viewed on any device. It was built using HTML, CSS, and Bootstrap.",
+        w_img_url:"https://kunnikarb.github.io/portfolio-genesis/",
         w_img:project3_img
     },
     {
         w_no:4,
         w_name:"Web design",
+        w_desc:"Web development is the process of building, programming...",
+        w_note:"Note: It is a responsive website that can be viewed on any device. ",
+        w_built:"Built using HTML, CSS, and JavaScript",
+        w_more:"This project is an agency design website that showcases the services of the agency. The website is responsive and can be viewed on any device. It was built using HTML, CSS, and Bootstrap.",
+        w_img_url:"https://kunnikarb.github.io/bootstrap/",
         w_img:project4_img
     },
     {
         w_no:5,
         w_name:"Web design",
+        w_desc:"This project is a portfolio website",
+        w_note:"Note: It is a responsive website that can be viewed on any device. ",
+        w_built:"Built using HTML, CSS, and JavaScript",
+        w_more:"This project is an agency design website that showcases the services of the agency. The website is responsive and can be viewed on any device. It was built using HTML, CSS, and Bootstrap.",
+        w_img_url:"https://kunnikarb.github.io/bootstrap/",
         w_img:project5_img
     },
     {
         w_no:6,
-        w_name:"Web design",
+        w_name:"Genesis Portfolio",
+        w_desc:"This project is a portfolio website",
+        w_note:"Note: It is a responsive website that can be viewed on any device. ",
+        w_built:"Built using HTML, CSS, and JavaScript",
+        w_more:"This project is an agency design website that showcases the services of the agency. The website is responsive and can be viewed on any device. It was built using HTML, CSS, and Bootstrap.",
+        w_img_url:"https://kunnikarb.github.io/bootstrap/",
         w_img:project6_img
     },
 ]
