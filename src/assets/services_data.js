a6f206202d1ec4e912b10604e54eb994d9fe6c56
Supanee <kunnikarboon@gmail.com>
2024-05-27 23:10:09 +0200

    modified

--- src/assets/services_data.js
+++ src/assets/services_data.js
@@ -1,34 +1,34 @@
 const Services_Data = [
-    {
-        s_no:"01",
-        s_name:"Web design",
-        s_desc:"Get landing pages designed from scratch or based on existing materials with thoughtful UI/UX wireframes and designs."
-    },
-    {
-        s_no:"02",
-        s_name:"Graphics design",
-        s_desc:"Web development is the process of building, programming..."
-    },
-    {
-        s_no:"03",
-        s_name:"Social media design",
-        s_desc:"Get original designs for your social media channels. Static, animated or video, from Instagram to YouTube or Facebook."
-    },
-    {
-        s_no:"04",
-        s_name:"App design",
-        s_desc:"Web development is the process of building, programming..."
-    },
-    {
-        s_no:"05",
-        s_name:"Digital marketing",
-        s_desc:"Empower your business with data-driven digital marketing initiatives, plans, and exceptional insights from our team of expert consultants."
-    },
-    {
-        s_no:"06",
-        s_name:"Motion Design",
-        s_desc:"Get on-brand motion graphics designed to enhance your website, digital campaigns, presentations and ads."
-    },
+  {
+      s_no:"01",
+      s_name:"Web design",
+      s_desc:"Get landing pages designed from scratch or based on existing materials with thoughtful UI/UX wireframes and designs."
+  },
+  {
+      s_no:"02",
+      s_name:"Graphics design",
+      s_desc:"Web development is the process of building, programming..."
+  },
+  {
+      s_no:"03",
+      s_name:"Social media design",
+      s_desc:"Get original designs for your social media channels. Static, animated or video, from Instagram to YouTube or Facebook."
+  },
+  {
+      s_no:"04",
+      s_name:"App design",
+      s_desc:"Web development is the process of building, programming..."
+  },
+  {
+      s_no:"05",
+      s_name:"Digital marketing",
+      s_desc:"Empower your business with data-driven digital marketing initiatives, plans, and exceptional insights from our team of expert consultants."
+  },
+  {
+      s_no:"06",
+      s_name:"Motion Design",
+      s_desc:"Get on-brand motion graphics designed to enhance your website, digital campaigns, presentations and ads."
+  },
 ]
- 
+
 export default Services_Data;
