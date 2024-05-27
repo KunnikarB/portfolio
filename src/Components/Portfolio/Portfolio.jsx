3f45ab99ea481a1a1252b8805aa5ac9a3b974ece
Supanee <kunnikarboon@gmail.com>
2024-05-27 20:32:24 +0200

    modified Portfolio

--- src/Components/Portfolio/Portfolio.jsx
+++ src/Components/Portfolio/Portfolio.jsx
@@ -1,31 +1,41 @@
-
+import { useState } from 'react';
 import './Portfolio.css';
 import Pattern from '../../assets/theme_pattern.svg';
 import mywork_data from '../../assets/mywork_data';
-import arrow_icon from '../../assets/arrow_icon.svg';
+import { MdExpandMore } from "react-icons/md";
+
 
 
 
 const  Portfolio = () => {
+
+
   return (
     <div className='mywork'>
       <div className='mywork-title'>
         <h1>Portfolio</h1>
         <img src= {Pattern} alt='' />
       </div>
-
-      <div className='mywork-container'>
-        { mywork_data.map((work,index) => {
-          return (
-              <img key={index} src={work.w_img} alt='' />
-          )
-        })}
-      </div>
-
-      <div className='mywork-showmore'>
-        <p>Show more</p>
-        <img src={arrow_icon} alt='' />
-      </div>
+     
+        <div className='mywork-show'>
+          { mywork_data.map((work,index) => {
+            return (
+              <div className='mywork-container'  >
+                <h2>{work.w_name}</h2>
+                <p>{work.w_desc}</p>
+                <p>{work.w_note}</p>
+                <p>{work.w_built}</p>
+                <div className='mywork-showmore'> 
+                <p onClick={work.w_more}>showmore<MdExpandMore /> 
+                </p>
+                </div>
+                <a href={work.w_img_url}><img key={index} src={work.w_img} alt='Link image to my project' title='Click to see my project' /></a>
+              </div>
+            )
+          })}
+        </div>
+      
+      
     </div>
   );
 };
