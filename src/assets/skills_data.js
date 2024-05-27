ec67cd849674d3e0e2cda7fd4b06b30dfe66936e
Supanee <kunnikarboon@gmail.com>
2024-05-28 01:10:43 +0200

    add new data

---
+++ src/assets/skills_data.js
@@ -0,0 +1,42 @@
+const skills_data = [
+  {
+      s_no:"01",
+      s_name: "HTML & CSS",
+      s_about:"HTML (HyperText Markup Language) and CSS (Cascading Style Sheets) are the foundation of websites in that they provide structure and styling to a web page.",
+     
+     
+  },
+
+  {
+    s_no:"02",
+    s_name: "Bootstrap",
+    s_about:"Be able to uses Bootstrap for create responsive mobile-first websites and web apps.",
+      
+  },
+  {
+    s_no:"03",
+    s_name: "JavaScript",
+    s_about:"Be able to write JavaScript to update and change both HTML and CSS. Uses JavaScript to calculate, manipulate and validate data.",
+
+      
+  },
+  {
+    s_no:"04",
+    s_name: "React js",
+    s_about:"Be able to create a single-page application (SPA). Uses vite to create reusable components and manage the state of the application.",
+
+  },
+  {
+    s_no:"05",
+    s_name: "Git & GitHub",
+    s_about:"Be able to use Git and GitHub to manage and store code. Uses Git to track changes in code and GitHub to store code.",
+  },
+  {
+    s_no:"06",
+    s_name: "Figma",
+    s_about:"Be able to use Figma to create wireframes and mockups of websites and web apps. Uses Figma to create a design system and prototype of the application.",
+  },
+  
+]
+
+export default skills_data;
---
+++ src/assets/works_data.js
@@ -0,0 +1,48 @@
+const works_data = [
+  {
+      w_no:"01",
+      w_name: "Frontend Developer",
+      w_employment:"Freelance web developer",
+      w_date:"I am currently working as this role",
+     
+  },
+
+  {
+      w_no:"02",
+      w_name:"K-Vintage Boutique",
+      w_employment:"Preloved Clothing Store",
+      w_date:"Sep 2021 - Dec 2022",
+
+      
+  },
+  {
+    w_no:"03",
+    w_name:"Forever Living Products Scandinavia AB",
+    w_employment:"Assistant Supervisor nutrition and beauty products",
+    w_date:"Feb 2012 - Jan 2016",
+
+      
+  },
+  {
+    w_no:"04",
+    w_name:"Freelife International (Thailand)",
+    w_employment:"Independent Marketing Executive",
+    w_date:"Jan 2010 - March 2012",
+
+  },
+  {
+    w_no:"05",
+    w_name:"Sea & Aero Logistics Co.Ltd.",
+    w_employment:"Sale freight forwarder internship",
+    w_date:"Jan 2010 - April 2010",
+  },
+  {
+      w_no:"06",
+      w_name:"Nutritional Health & Supplements Company",
+      w_employment:"Sale Freelance",
+      w_date:"May 2000 - Dec 2002",
+  },
+  
+]
+
+export default works_data;
