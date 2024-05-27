cd27a3f996ee32b14e7c48bacc1f71a508a66740
Supanee <kunnikarboon@gmail.com>
2024-05-27 20:31:17 +0200

    modified Footer.css

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -2,27 +2,18 @@
 import './Footer.css';
 import logo from '../../assets/logo.jpg';
 import user_icon from '../../assets/user_icon.svg';
+import { FaGithub } from "react-icons/fa";
+import { FaLinkedinIn } from "react-icons/fa6";
+import { FaInstagram } from "react-icons/fa6";
+
 
 const Footer = () => {
   return (
     <div className='footer'>
       <div className='footer-top'>
-        <div className='footer-top-left'>
           <img src={logo} alt='logo' />
-          <p>KB Creative Developer.</p>
-        </div>
-        <div className='footer-top-right'>
-          <div className='footer-email-input'>
-            <img src={user_icon} alt='user icon' />
-            <input type='email' placeholder='Enter your email' />
-
-          </div>
-
-          <div className='footer-subscribe'>Subscribe</div>
-
-          </div>
-
-        </div>
+          <p >KB Creative Developer.</p> 
+      </div>
         <hr />
 
         <div className='footer-bottom'>
@@ -30,9 +21,9 @@ const Footer = () => {
             <p className='footer-bottom-left'>© 2024 KunnikarB All rights reserved</p>
           
             <div className='footer-bottom-right'>
-              <p>Privacy Policy</p>
-              <p>Terms of Service</p>
-              <p>Connect with me</p>
+              <a href='https://www.linkedin.com/in/kunnikar/'><FaGithub /></a>
+              <a href='https://github.com/KunnikarB'><FaLinkedinIn /></a>
+              <a href='https://www.instagram.com/kunnikar_b/'><FaInstagram /></a>
             </div>
  
         </div>
