ea82e95f9f3a91bb063a0827c928a8d7bc510c6f
Supanee <kunnikarboon@gmail.com>
2024-05-28 09:50:05 +0200

    modified

--- src/assets/mywork_data.js
+++ src/assets/mywork_data.js
@@ -1,6 +1,6 @@
 import project1_img from '../assets/KB-Creativ.png'
 import project2_img from '../assets/Bhromaon.png'
-import project3_img from '../assets/Genesis.png'
+import project3_img from '../assets/Taco.png'
 import project4_img from '../assets/Invitation.png'
 import project5_img from '../assets/Thai-cuisine.png'
 import project6_img from '../assets/iBerry.png'
@@ -28,12 +28,12 @@ const mywork_data = [
     },
     {
         w_no:3,
-        w_name:"Genesis Portfolio",
-        w_desc:"Web development is the process of building, programming...",
-        w_note:"Note: It is a responsive website that can be viewed on any device. ",
-        w_built:"Built using HTML, CSS, and JavaScript",
-        w_more:"This project is an agency design website that showcases the services of the agency. The website is responsive and can be viewed on any device. It was built using HTML, CSS, and Bootstrap.",
-        w_img_url:"https://kunnikarb.github.io/portfolio-genesis/",
+        w_name:"Frank & Bruno Taco",
+        w_desc:"This project is a Taco restaurant website.",
+        w_note:"Note: It is a responsive website. ",
+        w_built:"Built using HTML and CSS animation.  ",
+        w_more:"This project is a Taco restaurant website that showcases the menu. The website is responsive and can be viewed on any device.",
+        w_img_url:"https://kunnikar-b.w3spaces.com/",
         w_img:project3_img
     },
     {
